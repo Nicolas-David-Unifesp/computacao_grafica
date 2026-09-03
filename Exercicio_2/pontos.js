389f6686633545c6f5df6a95f5e71a3df430c200
Nicolas <nicolas.david@unifesp.br>
2026-09-03 15:41:23 -0300

    Criando a função de Breseham

--- Exercicio_2/pontos.js
+++ Exercicio_2/pontos.js
@@ -5,7 +5,6 @@ const gl = canvas.getContext("webgl2");
 
 */ 
 
-
 if (!gl) {
     throw new Error("WebGL 2 não é suportado.");
 }
@@ -21,6 +20,31 @@ const webglCoordinates =
         "webglCoordinates"
     );
 
+
+
+ // --------------------------------------------------
+// 0. Funções
+// --------------------------------------------------
+
+function bresenham(x0, y0, x1, y1) {
+    const points = [];
+    let dx = Math.abs(x1 - x0);
+    let dy = Math.abs(y1 - y0);
+    let sx = x0 < x1 ? 1 : -1;
+    let sy = y0 < y1 ? 1 : -1;
+    let err = dx - dy;
+ 
+    let x = x0, y = y0;
+ 
+    while (true) {
+        points.push({ px: x, py: y });
+        if (x === x1 && y === y1) break;
+        const e2 = 2 * err;
+        if (e2 > -dy) { err -= dy; x += sx; }
+        if (e2 <  dx) { err += dx; y += sy; }
+    }
+    return points;
+}   
 
 // --------------------------------------------------
 // 1. VERTICES
