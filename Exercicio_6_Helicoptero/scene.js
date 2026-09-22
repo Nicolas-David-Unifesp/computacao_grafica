3d3b8e97e048827ca27c492dd9f79f1e2c2a60f9
Nicolas <nicolas.david@unifesp.br>
2026-09-22 16:48:45 -0300

    Efetuando uma nova movimentação de transladar em x.

--- Exercicio_6_Helicoptero/scene.js
+++ Exercicio_6_Helicoptero/scene.js
@@ -41,14 +41,28 @@ class Scene {
         if (this.keys["ArrowDown"])  this.rotX += this.speed;
         if (this.keys["ArrowRight"]) this.rotY += this.speed;
         if (this.keys["ArrowLeft"])  this.rotY -= this.speed;
+
+        // Translação (W A S D) — mover o helicóptero pela tela
+        if (this.keys["w"] || this.keys["W"]) this.ty += this.speed; // cima
+        if (this.keys["s"] || this.keys["S"]) this.ty -= this.speed; // baixo
+        if (this.keys["a"] || this.keys["A"]) this.tx -= this.speed; // esquerda
+        if (this.keys["d"] || this.keys["D"]) this.tx += this.speed; // direita
     }
+
+    
 
     update() {
         this.handleInput();
         this.theta += 0.05;
 
         // rotação global do helicóptero
-        const R = m4.multiply(m4.yRotation(this.rotY), m4.xRotation(this.rotX));
+        const rotGlobal = m4.multiply(m4.yRotation(this.rotY), m4.xRotation(this.rotX));
+
+        // translação global (W A S D)
+        const T = m4.translation(this.tx, this.ty, 0.0);
+
+        // transform global = translação * rotação
+        const R = m4.multiply(T, rotGlobal);
 
         this.helicopterBody.update(R);
         this.helicopterTopShaft.update(R);
@@ -57,7 +71,7 @@ class Scene {
         const rotTop = m4.yRotation(this.theta);
         this.helicopterPropellers.update(m4.multiply(R, rotTop));
 
-        const caudaCentro    = m4.translation( 0.7, 0.0,  0.06);
+        const caudaCentro = m4.translation( 0.7, 0.0,  0.06);
         const caudaCentroInv = m4.translation(-0.7, 0.0, -0.06);
         const rotTail = m4.zRotation(this.theta);
         const localTail = m4.multiply(m4.multiply(caudaCentro, rotTail), caudaCentroInv);
@@ -66,7 +80,7 @@ class Scene {
 
     draw() {
 
-        const gl = this.gl;         // <-- usar this.gl
+        const gl = this.gl;        
         const program = this.program;
 
         gl.clear(
